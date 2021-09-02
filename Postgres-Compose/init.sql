c550a2fe48e11e91cb7d17f6314c729cf70675f8
Rafael Rabelo <52937689+RafaelMR86@users.noreply.github.com>
2021-09-01 21:36:08 -0300

    Adicionando compose

---
+++ Postgres-Compose/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE my_table (
+    id INT PRIMARY KEY NOT NULL,
+    value TEXT NOT NULL
+);
+
+INSERT INTO
+    my_table(id, value)
+VALUES
+    (1, 'DADO 1'),
+    (2, 'DADO 2'),
+    (3, 'FUNCIONOU');
